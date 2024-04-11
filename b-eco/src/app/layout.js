004db326f5775f766f0d42e265388bee320e9a72
Juan Salazar <jjsc2003@live.com>
2024-04-11 14:34:52 -0600

    Try adding analytics

--- b-eco/src/app/layout.js
+++ b-eco/src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics/>
     </html>
   )
 }
